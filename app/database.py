cda46ffa22f4ed36dfb3707ac4804e3386b3f5c4
nguyenthanhak8-hue <nguyenthanhak8@gmail.com>
2025-08-10 13:00:16 +0700

    First commit

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 #SQLALCHEMY_DATABASE_URL = "postgresql://postgres:123@localhost/kiosk_db"
-SQLALCHEMY_DATABASE_URL = "postgresql://hcc_test_n4kv_user:yIzoeFfgAKJeCQoAtZbe5UXqIbSc1KAQ@dpg-d25cukadbo4c73bfspg0-a.oregon-postgres.render.com/hcc_test_n4kv"
+SQLALCHEMY_DATABASE_URL = "postgresql://lstd_user:gCMJ45hF04pyF6LJY2WRSNY56WiZYOtS@dpg-d2c307idbo4c73baolq0-a/lstd"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
